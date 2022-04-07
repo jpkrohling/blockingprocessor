40bd62e8a3d1a38bac9c8dcb1f1d8463a041edf3
Juraci Paixão Kröhling <juraci@kroehling.de>
2022-04-07 19:09:22 +0200

    block
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- processor.go
+++ processor.go
@@ -2,10 +2,12 @@ package blockingprocessor
 
 import (
 	"context"
+	"time"
 
 	"go.opentelemetry.io/collector/model/pdata"
 )
 
 func processTraces(_ context.Context, traces pdata.Traces) (pdata.Traces, error) {
+	time.Sleep(time.Minute) // will block for one minute
 	return traces, nil
 }
